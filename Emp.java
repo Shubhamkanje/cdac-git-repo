51d70d18a8d66d3ce82094eba3ed59e3ab042812
Shubhamkanje <97588232+Shubhamkanje@users.noreply.github.com>
2022-01-13 18:23:41 +0530

    Add files via upload

---
+++ Emp.java
@@ -0,0 +1,27 @@
+class Emp
+{
+	private int id;
+	private String name;
+	private int salary;
+	
+	Emp()
+	{
+		id = 101;
+		name = "Dhiraj";
+		salary = 20000;
+	}
+	Emp(int d,String n,int s)
+	{
+		id = d;
+		name = n;
+	    salary = s;
+	}
+	
+	 public void showEmpDetails(){
+		  
+		  System.out.println("Employee id = " + id);
+		  System.out.println("Employee name = " + name);
+		  System.out.println("Employee salary = " + salary);
+		 }
+
+}
